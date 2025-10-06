92641163427ce3fec2815477d5bd2318be0b3d49
Facundo Gerry <facuto365pro@gmail.com>
2025-10-06 17:17:12 -0300

    Fixes cause i forgor to do them
    mainly removed unused assets

--- Assets/Scripts/Ui/UiLevelCompleted.cs
+++ Assets/Scripts/Ui/UiLevelCompleted.cs
@@ -1,12 +1,17 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UiLevelCompleted : MonoBehaviour
 {
+    [SerializeField] private CoinsDataSo coinsData;
     [SerializeField] private GameObject levelCompleted;
     [SerializeField] private Button btnReplay;
     [SerializeField] private Button btnMainMenu;
+    [SerializeField] private GameObject coinsCounter;
+    [SerializeField] private TextMeshProUGUI coinsNum;
+    [SerializeField] private TextMeshProUGUI totalCoinsNum;
 
     private void Start()
     {
@@ -17,6 +22,9 @@ public class UiLevelCompleted : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Time.timeScale = 0f;
+        coinsNum.text = coinsData.coins.ToString("0");
+        totalCoinsNum.text = coinsData.totalCoins.ToString("0");
+        coinsCounter.SetActive(false);
         levelCompleted.SetActive(true);
     }
 
